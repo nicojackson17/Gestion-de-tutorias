6aa7012ed8dcd85e83be2f6c50e8cf275e8e7dfb
lugiolu124 <lugiolu124@gmail.com>
2026-08-21 10:08:01 -0300

    inicio 2
    finalizado probablemente (faltan coneccion de ramas

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ function iniciarSesion() {
     let nombre = document.getElementById("nombre").value;
     let contraseña = document.getElementById("contraseña").value;
 
-    if (nombre == "" || contraseña == "") {
-        document.getElementById("mensaje").innerHTML = "Completá todos los campos";
-    } else {
+    if (nombre == "Giovana" && contraseña == "1234") {
         document.getElementById("mensaje").innerHTML = "Inicio de sesión correcto";
+    } else {
+        document.getElementById("mensaje").innerHTML =
+            "El usuario o contraseña no existen o son incorrectos";
     }
 
 }
